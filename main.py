b39628d87529cb2970d87fbed83f19b6504aae0c
bsayfiddin2197 <bsayfiddin2197@gmail.com>
2025-11-29 17:44:49 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+# 11
+ism = lambda im: "a" in im
+
+res = ism("Sayfiddin")
+print(res)
+
+# 12
+katta3 = lambda x: x.capitalize()
+
+res = katta3("sayfiddin")
+print(res)
+
+# 13
+teng = lambda a, b: a == b
+
+res = teng(12, 12)
+print(res)
+
+# 14
+teng2 = lambda a, b: a == b
+
+res = teng2(16, 16)
+print(res)
+
+# 15
+bol = lambda x: x // 2
+
+res = bol(18)
+print(res)
